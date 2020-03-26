1599c7e282ecbda59ecda9c04957a8fbc47e2a1b
goldinmaxim <maxim_goldin@bk.ru>
2020-03-26 22:10:05 +0300

    Create mapper.py

---
+++ mapper.py
@@ -0,0 +1,8 @@
+import string
+import fileinput
+
+for line in fileinput.input():
+    data = line.strip().split("\t")
+    if len(data) == 6:
+        ip, userid, country, bannerid, payout = data
+        print "{0}\t{1}".format(country, payout)
